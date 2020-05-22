e17d0808ad485a74519909a984cf18d4b363f651
unknown <kidosol2169@gmail.com>
2020-05-22 15:55:12 +0800

    updated feature

--- go.js
+++ go.js
@@ -1,3 +1,5 @@
+/* bugishes: ko */
+
 const canvas_size = 400;
 
 const size = 19;
@@ -23,18 +25,37 @@ function drawBoard() {
     line(cell_size/2, y*cell_size+(cell_size/2), canvas_size-(cell_size/2), y*cell_size+(cell_size/2))
   }
   
+  strokeWeight(5);
+  for (let star of [[3, 3], [15, 3], [15, 15], [3, 15], [9, 3], [15, 9], [9, 15], [3, 9], [9, 9]]) {
+    point(star[0]*cell_size+(cell_size/2), star[1]*cell_size+(cell_size/2));
+  }
+  
+  strokeWeight(1);
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
       const cell = board[x][y];
       if (cell[0] === 'black') {
-        strokeWeight(1);
+        stroke(0);
         fill(0);
         ellipse(x*cell_size+(cell_size/2), y*cell_size+(cell_size/2), cell_size*0.9);
+        fill(60);
+        ellipse(x*cell_size+(cell_size*0.45), y*cell_size+(cell_size*0.45), cell_size*0.7);
+        
+        noStroke();
+        fill(200);
+        ellipse(x*cell_size+(cell_size*0.3), y*cell_size+(cell_size*0.3), cell_size*0.2);
       }
       else if (cell[0] === 'white') {
+        stroke(220);
         strokeWeight(1);
-        fill(255);
+        fill(235);
         ellipse(x*cell_size+(cell_size/2), y*cell_size+(cell_size/2), cell_size*0.9);
+        fill(248);
+        ellipse(x*cell_size+(cell_size*0.45), y*cell_size+(cell_size*0.45), cell_size*0.7);
+        
+        noStroke();
+        fill(255);
+        ellipse(x*cell_size+(cell_size*0.3), y*cell_size+(cell_size*0.3), cell_size*0.3);
       }
     }
   }
@@ -49,16 +70,16 @@ function isGroupAlive(x, y) {
     const cell = board[x][y]
     
     if (x !== 0 && board[x-1][y][0] === null) alive = true;
-    else if (x !== 0 && board[x-1][y][0] === cell[0] && stack.includes([x-1, y].toString)) check(x-1, y);
+    else if (x !== 0 && board[x-1][y][0] === cell[0] && !stack.includes([x-1, y].toString())) check(x-1, y);
     
     if (x !== size-1 && board[x+1][y][0] === null) alive = true;
-    else if (x !== size-1 && board[x+1][y][0] === cell[0] && stack.includes([x+1, y].toString)) check(x+1, y);
+    else if (x !== size-1 && board[x+1][y][0] === cell[0] && !stack.includes([x+1, y].toString())) check(x+1, y);
     
     if (y !== 0 && board[x][y-1][0] === null) alive = true;
-    else if (y !== 0 && board[x][y-1][0] === cell[0] && stack.includes([x, y-1].toString)) check(x, y-1);
+    else if (y !== 0 && board[x][y-1][0] === cell[0] && !stack.includes([x, y-1].toString())) check(x, y-1);
     
     if (y !== size-1 && board[x][y+1][0] === null) alive = true;
-    else if (y !== size-1 && board[x][y+1][0] === cell[0] && stack.includes([x, y+1].toString)) check(x, y+1);
+    else if (y !== size-1 && board[x][y+1][0] === cell[0] && !stack.includes([x, y+1].toString())) check(x, y+1);
   }
   
   check(x, y);
@@ -80,11 +101,24 @@ function removeGroup(x, y) {
   return;
 }
 
+let is_ko = false;
+
 function updateBoard(x, y) {
+  let captured_opponent = false;
   for (const adjacent of [[x-1, y], [x+1, y], [x, y-1], [x, y+1]]) {
-    console.log(adjacent, isGroupAlive(adjacent[0], adjacent[1]))
-    if (board[adjacent[0]][adjacent[1]] !== void 0 && !isGroupAlive(adjacent[0], adjacent[1])) removeGroup(adjacent[0], adjacent[1]);
+    if (board[adjacent[0]] !== void 0 && board[adjacent[0]][adjacent[1]] !== void 0 && !isGroupAlive(adjacent[0], adjacent[1]) && adjacent.toString() !== is_ko) {
+      removeGroup(adjacent[0], adjacent[1]);
+      captured_opponent=true;
+    }
   }
+  
+  // check for: suicide move and ko
+  //if ((!isGroupAlive(x, y) && !captured_opponent) || (is_ko &&is_ko.toString() === [x, y].toString())) {
+  //  board[x][y][0] = null;
+  //  current_player = (current_player == 'black') ? 'white' : 'black';
+  //}else if (!isGroupAlive(x, y) && captured_opponent) {
+  //  is_ko
+  //}
 }
 
 let current_player = 'black';
@@ -98,5 +132,11 @@ function mousePressed() {
     current_player = (current_player == 'black') ? 'white' : 'black';
     updateBoard(board_x, board_y);
     drawBoard();
+    
+    stroke(255, 50, 50);
+    strokeWeight(3);
+    noFill();
+    ellipse(board_x*cell_size+(cell_size/2), board_y*cell_size+(cell_size/2), cell_size*0.6);
+
   }
 }
